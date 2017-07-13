7ce3283f081974b43a13fa80943fef498d5977d3
jean boulet <jbbwebsolutions@gmail.com>
2017-07-13 19:11:27 -0400

    connecting to a database

---
+++ src/main/java/com/fanniemae/payroll/dao/CityRepository.java
@@ -0,0 +1,34 @@
+package com.fanniemae.payroll.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.fanniemae.payroll.model.City;
+
+public class CityRepository extends AbstractSQLDAO 
+    implements IQuery<City> {
+
+	ArrayList<City> list = new ArrayList<>();
+	
+	@Override
+	protected void results(ResultSet rs) throws SQLException {
+		
+		int id = rs.getInt("id");
+		String name = rs.getString("name");
+		int population = rs.getInt("population");
+		list.add(  new City(id, name, population));
+		
+	}
+	
+	@Override	
+	public ArrayList<City> findAll() {
+		
+		String sql = "select Id, name, population " + " from city";
+		super.process(sql);
+		// TODO Auto-generated method stub
+		return list;
+	}
+	
+
+}
